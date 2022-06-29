9d6ee13673c0315d7f2e1d1af6754a9c4fced27d
manishchaulagain1 <manish.chaulagain@apexcollege.edu.np>
2022-06-29 20:07:09 +0545

    Playlist Completed Upto 10

--- LMS/LMS/urls.py
+++ LMS/LMS/urls.py
@@ -1,7 +1,10 @@
 
+from xml.dom.minidom import Document
 from django.contrib import admin
 from django.urls import path, include
 from .import views, user_login
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -18,4 +21,5 @@ urlpatterns = [
     path('doLogin', user_login.DO_LOGIN, name='doLogin'),
     path('accounts/profile', user_login.PROFILE, name='profile'),
     path('accounts/profile/update', user_login.PROFILE_UPDATE, name='profile_update'),
-]
+
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- LMS/LMS/views.py
+++ LMS/LMS/views.py
@@ -1,10 +1,20 @@
+from multiprocessing import context
+from unicodedata import category
 from django.shortcuts import redirect,render
+from app.models import Categories,Course
 
 def BASE(request):
     return render(request,'base.html')
 
 def HOME(request):
-    return render(request,'Main/home.html')
+    category = Categories.objects.all().order_by('id')[0:5]
+    course = Course.objects.filter(status = 'PUBLISH').order_by('-id')
+
+    context = { 
+        'category':category,
+        'course':course,
+    }
+    return render(request,'Main/home.html',context)
 
 def SINGLE_COURSE(request):
     return render(request,'Main/single_course.html')
